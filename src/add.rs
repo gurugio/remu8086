6b5b3849098a4fe285c82f91704b2a9e23244222
Gioh Kim <gurugio@a.com>
2022-12-25 01:49:56 +0900

    add: refine do_add()

--- src/add.rs
+++ src/add.rs
@@ -15,25 +15,25 @@ use crate::cpucontext::{CpuContext, Instruction, OperFlag};
 
 pub fn do_add(context: &mut CpuContext, inst: &Instruction) {
     // We already know that inst.operation is "ADD".
+    // ADD operation always has dest and src operands.
 
-    // When left is register16
-    if let Some(left) = inst.dest.as_ref() {
-        if inst.src.is_none() {
-            panic!("wrong right");
-        }
-
-        let target_reg = &left.field;
-        let cur_val = context.read_reg(target_reg);
-        context.write_reg(
-            target_reg,
-            cur_val
-                + inst
-                    .src
-                    .as_ref()
-                    .map(|opnd| opnd.field.parse::<u16>().unwrap())
-                    .unwrap(),
-        );
-    } else {
-        todo!();
+    if inst.dest.is_none() || inst.src.is_none() {
+        // TODO: return error with anyhow
+        panic!("wrong operands: Add requires both operands");
     }
+
+    let dest = inst.dest.as_ref().unwrap();
+    let src = inst.src.as_ref().unwrap();
+
+    // TODO: check type of dest and src
+    add_reg16_imm16(context, &dest.field, &src.field);
 }
+
+fn add_reg16_imm16(context: &mut CpuContext, reg: &str, imm: &str) {
+    // if dest is reg16 and src is imm16
+    let cur_val = context.read_reg(reg);
+    context.write_reg(reg, cur_val + imm.parse::<u16>().unwrap());
+    // TODO: update context.flag
+}
+
+// TODO: add unittest for each case
