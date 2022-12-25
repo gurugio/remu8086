31a911204dc01254b658253dd471a89d2b4253cf
Gioh Kim <gurugio@gmail.com>
2022-12-25 19:53:24 +0900

    test case

--- src/add.rs
+++ src/add.rs
@@ -1,12 +1,12 @@
 use crate::cpucontext::{CpuContext, Instruction, OperFlag};
 
 /// add series
-/// ADD r/m8, reg8 $00
-/// ADD r/m16, reg16 $01
-/// ADD reg8, r/m8 $02
-/// ADD reg16, r/m16 $03
 /// ADD AL, imm8 $04
 /// ADD AX, imm16 $05
+/// ADD reg8, r/m8 $02
+/// ADD reg16, r/m16 $03
+/// ADD r/m8, reg8 $00
+/// ADD r/m16, reg16 $01
 /// ADD r/m8, imm8 $80 xx000xxx (ModR/M byte)
 /// ADD r/m16, imm16 $81 xx000xxx (ModR/M byte)
 /// ADD r/m16, imm8 $83 xx000xxx (ModR/M byte)
@@ -25,8 +25,18 @@ pub fn do_add(context: &mut CpuContext, inst: &Instruction) {
     let dest = inst.dest.as_ref().unwrap();
     let src = inst.src.as_ref().unwrap();
 
-    // TODO: check type of dest and src
+    // TODO: check type of dest and src and select handler
     add_reg16_imm16(context, &dest.field, &src.field);
+
+    // dest  src
+    // al    imm8
+    // ax    imm16
+    // reg8  reg8
+    // reg8  mem8
+    // reg16 reg16
+    // reg16 mem16
+    // 
+
 }
 
 fn add_reg16_imm16(context: &mut CpuContext, reg: &str, imm: &str) {
@@ -37,3 +47,14 @@ fn add_reg16_imm16(context: &mut CpuContext, reg: &str, imm: &str) {
 }
 
 // TODO: add unittest for each case
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_add_reg16_imm16() {
+        let mut init_context = crate::cpucontext::CpuContext::default();
+        add_reg16_imm16(&mut init_context, "ax", "1");
+        assert_eq!(init_context.ax, 1);
+    }
+}
