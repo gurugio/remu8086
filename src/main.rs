45b36a325a79c962736c839b8d05ec8d8b91e859
Gioh Kim <gurugio@l.com>
2024-11-06 19:03:32 +0900

    Change parser: parse imm, reg8, reg16, memx, mem8, mem16
    There is no need to parse operand because it has to check the inner
    Pair of operand and find out reg and imm.
    It can parse imm, reg, mem directly because this patch changes
    operand rule to the Silent rule.
    
    And this patch adds reg8, reg16 and memx/8/16 rules.

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,7 @@ use std::fs;
 #[grammar = "assembly.pest"] // grammar file
 struct AssemblyParser;
 
-/// TODO
-/// 1. parse two operands
+/// 1. parse two operands: operand => register
 /// 2. call handle_mov function in mov.rs file (will be created).
 /// - handle_mov parses the operands and calls mov_reg_reg(), mov_reg_imm() and etc
 /// 3. make parse_add/sub/mul/div/jmp/cmp
@@ -16,7 +15,7 @@ struct AssemblyParser;
 fn parse_mov(operands: &mut Pairs<'_, Rule>) {
     println!("parse_mov");
 
-    // Below prints each operands as "Operand" but we should know the type of operand: register, number or label.
+    // Below prints each operands as "Operand" but we should know the type of operand: register, imm or label.
     let first_operand = operands.next().unwrap();
     println!(
         "first: rule={:?} text={}",
@@ -35,15 +34,9 @@ fn parse_mov(operands: &mut Pairs<'_, Rule>) {
 }
 
 fn main() {
-    /*let pairs = AssemblyParser::parse(Rule::instruction, "mov ax, bx")
-    .expect("Failed to parse")
-    .next()
-    .unwrap();*/
-
-    println!("########### try to read a file");
     let unparsed_file = fs::read_to_string("example.as").expect("cannot read file");
     let file = AssemblyParser::parse(Rule::program, &unparsed_file)
-        .expect("unsuccessful parse") // unwrap the parse result
+        .expect("Failed to parse a file with Rule::program rule") // unwrap the parse result
         .next()
         .unwrap(); // get and unwrap the `file` rule; never fails
     for line in file.into_inner() {
@@ -64,24 +57,19 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_number() {
-        /*
-        Rule::number is a silent rule. Pair has only Rule::hex or Rule::digit.
-        Then we can parse hex or digit into hex-number or digit-number because we know what it is.
-        */
-        let hex = AssemblyParser::parse(Rule::number, "0x1234")
+    fn test_imm() {
+        let hex = AssemblyParser::parse(Rule::imm, "0x1234")
             .unwrap()
             .next()
             .unwrap();
-        assert_eq!(Rule::hex, hex.as_rule());
+        assert_eq!(Rule::imm, hex.as_rule());
         assert_eq!("0x1234", hex.as_str());
+    }
 
-        let digit = AssemblyParser::parse(Rule::number, "1234")
-            .unwrap()
-            .next()
-            .unwrap();
-        assert_eq!(Rule::digit, digit.as_rule());
-        assert_eq!("1234", digit.as_str());
+    #[test]
+    #[should_panic(expected = "Decimal is not allowed")]
+    fn test_imm_failure() {
+        AssemblyParser::parse(Rule::imm, "1234").expect("Decimal is not allowed");
     }
 
     #[test]
@@ -90,8 +78,15 @@ mod tests {
             .unwrap()
             .next()
             .unwrap();
-        assert_eq!(Rule::register, reg.as_rule());
+        assert_eq!(Rule::reg16, reg.as_rule());
         assert_eq!("ax", reg.as_str());
+
+        let reg = AssemblyParser::parse(Rule::register, "al")
+            .unwrap()
+            .next()
+            .unwrap();
+        assert_eq!(Rule::reg8, reg.as_rule());
+        assert_eq!("al", reg.as_str());
 
         // The Rule::register is the terminal syntax. So there is no inner rules.
         let inner = reg.into_inner().next();
@@ -104,29 +99,29 @@ mod tests {
             .unwrap()
             .next()
             .unwrap();
-        assert_eq!(Rule::operand, operand.as_rule());
+        assert_eq!(Rule::reg16, operand.as_rule());
         assert_eq!("ax", operand.as_str());
 
-        // Rule::operand includes register and number.
-        let inner = operand.into_inner().next().unwrap();
-        assert_eq!(Rule::register, inner.as_rule());
-        assert_eq!("ax", inner.as_str());
+        // Rule::operand includes register and imm.
+        //let inner = operand.into_inner().next().unwrap();
+        //assert_eq!(Rule::register, inner.as_rule());
+        //assert_eq!("ax", inner.as_str());
 
         let operand = AssemblyParser::parse(Rule::operand, "0x1234")
             .unwrap()
             .next()
             .unwrap();
-        assert_eq!(Rule::operand, operand.as_rule());
+        assert_eq!(Rule::imm, operand.as_rule());
         assert_eq!("0x1234", operand.as_str());
 
-        // Rule::operand includes register and number.
-        let inner = operand.into_inner().next().unwrap();
-        assert_eq!(Rule::hex, inner.as_rule());
-        assert_eq!("0x1234", inner.as_str());
+        // Rule::operand includes register and imm.
+        //let inner = operand.into_inner().next().unwrap();
+        //assert_eq!(Rule::hex, inner.as_rule());
+        //assert_eq!("0x1234", inner.as_str());
     }
 
     #[test]
-    fn test_instruction() {
+    fn test_instruction_imm() {
         let instruction = AssemblyParser::parse(Rule::instruction, "mov ax, 0x100")
             .unwrap()
             .next()
@@ -134,31 +129,49 @@ mod tests {
         assert_eq!(Rule::mov, instruction.as_rule());
         assert_eq!("mov ax, 0x100", instruction.as_str());
 
-        /*
-        instruction type is Pair. The into_inner method returns Pairs that is an iterator on Pair of enclosed rules
-        mov = { "mov" + operand + ',' + operand } => Enclosed rules are operand and operand.
-        So below for loop returns
-        Rule: operand Text: ax
-        Rule: operand Text: 0x100
-        for pair in instruction.into_inner() {
-            println!("operand: Rule: {:?} Text={}", pair.as_rule(), pair.as_str());
-        }
-        */
         let mut inner = instruction.into_inner();
         let ax = inner.next().unwrap();
-        assert_eq!(Rule::operand, ax.as_rule());
+        assert_eq!(Rule::reg16, ax.as_rule());
         assert_eq!("ax", ax.as_str());
-        let ax_inner = ax.into_inner().next().unwrap();
-        assert_eq!(Rule::register, ax_inner.as_rule());
-        assert_eq!("ax", ax_inner.as_str());
 
         let imm = inner.next().unwrap(); // second operands is imm
-        assert_eq!(Rule::operand, imm.as_rule());
+        assert_eq!(Rule::imm, imm.as_rule());
         assert_eq!("0x100", imm.as_str());
 
-        let imm_inner = imm.into_inner().next().unwrap();
-        assert_eq!(Rule::hex, imm_inner.as_rule());
-        assert_eq!("0x100", imm_inner.as_str());
+        let instruction = AssemblyParser::parse(Rule::instruction, "mov al, 0x12")
+            .unwrap()
+            .next()
+            .unwrap();
+        assert_eq!(Rule::mov, instruction.as_rule());
+        assert_eq!("mov al, 0x12", instruction.as_str());
+
+        let mut inner = instruction.into_inner();
+        let al = inner.next().unwrap();
+        assert_eq!(Rule::reg8, al.as_rule());
+        assert_eq!("al", al.as_str());
+
+        let imm = inner.next().unwrap(); // second operands is imm
+        assert_eq!(Rule::imm, imm.as_rule());
+        assert_eq!("0x12", imm.as_str());
+    }
+
+    #[test]
+    fn test_instruction_reg() {
+        let instruction = AssemblyParser::parse(Rule::instruction, "mov ax, bx")
+            .unwrap()
+            .next()
+            .unwrap();
+        assert_eq!(Rule::mov, instruction.as_rule());
+        assert_eq!("mov ax, bx", instruction.as_str());
+
+        let mut inner = instruction.into_inner();
+        let ax = inner.next().unwrap();
+        assert_eq!(Rule::reg16, ax.as_rule());
+        assert_eq!("ax", ax.as_str());
+
+        let bx = inner.next().unwrap(); // second operands is imm
+        assert_eq!(Rule::reg16, bx.as_rule());
+        assert_eq!("bx", bx.as_str());
 
         // another instruction with a single operand
         let mul = AssemblyParser::parse(Rule::mul, "mul ax")
@@ -167,5 +180,58 @@ mod tests {
             .unwrap();
         assert_eq!(Rule::mul, mul.as_rule());
         assert_eq!("mul ax", mul.as_str());
+        let mut inner = mul.into_inner();
+        let ax = inner.next().unwrap();
+        assert_eq!(Rule::reg16, ax.as_rule());
+        assert_eq!("ax", ax.as_str());
+    }
+
+    #[test]
+    fn test_mem() {
+        let instruction = AssemblyParser::parse(Rule::instruction, "mov [0x1234], ax")
+            .unwrap()
+            .next()
+            .unwrap();
+        assert_eq!(Rule::mov, instruction.as_rule());
+        assert_eq!("mov [0x1234], ax", instruction.as_str());
+
+        let mut inner = instruction.into_inner();
+        let memx = inner.next().unwrap();
+        assert_eq!(Rule::memx, memx.as_rule());
+        assert_eq!("[0x1234]", memx.as_str());
+
+        let ax = inner.next().unwrap(); // second operands is imm
+        assert_eq!(Rule::reg16, ax.as_rule());
+        assert_eq!("ax", ax.as_str());
+
+        let instruction = AssemblyParser::parse(Rule::instruction, "mov ax, word ptr [0x1234]")
+            .unwrap()
+            .next()
+            .unwrap();
+        assert_eq!(Rule::mov, instruction.as_rule());
+        assert_eq!("mov ax, word ptr [0x1234]", instruction.as_str());
+
+        let instruction = AssemblyParser::parse(Rule::instruction, "mov al, byte ptr [0x12]")
+            .unwrap()
+            .next()
+            .unwrap();
+        assert_eq!(Rule::mov, instruction.as_rule());
+        assert_eq!("mov al, byte ptr [0x12]", instruction.as_str());
+    }
+
+    #[test]
+    fn test_program() {
+        let program = "mov ax, bx\nadd cx, 0x1234\njmp 1\n1:";
+        let file = AssemblyParser::parse(Rule::program, program)
+            .expect("Failed to parse a file with Rule::program rule") // unwrap the parse result
+            .next()
+            .unwrap(); // get and unwrap the `file` rule; never fails
+        let mut lines = file.into_inner();
+        let mov = lines.next().unwrap();
+        assert_eq!(Rule::mov, mov.as_rule());
+        let add = lines.next().unwrap();
+        assert_eq!(Rule::add, add.as_rule());
+        let jmp = lines.next().unwrap();
+        assert_eq!(Rule::jmp, jmp.as_rule());
     }
 }
