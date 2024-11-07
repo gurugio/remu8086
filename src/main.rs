aa076b15f451005f489671d72206885616cfa55d
Gioh Kim <gurugio@linecorp.com>
2024-11-07 23:44:33 +0900

    on progress: thinking about CpuContext

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,42 @@ use std::fs;
 
 call_handler_two!(mov, handle_mov);
 
+#[derive(Default)]
+struct CpuContext {
+    ax: u16,
+    bx: u16,
+    cx: u16,
+    dx: u16,
+    sp: u16,
+    bp: u16,
+    si: u16,
+    di: u16,
+    cs: u16,
+    ds: u16,
+    es: u16,
+    ss: u16,
+    ip: u16,
+}
+
+impl CpuContext {
+    fn new() -> Self {
+        Default::default()
+    }
+    // TODO: set_* functions to set each register16 and register8
+    // set_ax, set_al, set_ah, get_ax, get_al, get_ah
+    // set_bx, ...
+    // For example, "push ax" calls
+    // 1. ss = get_ss, sp = get_sp, ax = get_ax
+    // 2. set_sp(get_sp() - 2)
+    // Initial stack address: ss:sp = 0x0:0x0
+    // First input: ss:sp = 0xf:0xfffe
+    // Second input: ss:sp = 0xf:0xfffc
+    // ....0xf:0x0000
+    // ....0xe:0xfffe
+    // ....when sp gets underflow, ss is decreased.
+    // 3. [ss:sp] = ax
+}
+
 fn main() {
     let unparsed_file = fs::read_to_string("example.as").expect("cannot read file");
     let file = parser::AssemblyParser::parse(parser::Rule::program, &unparsed_file)
@@ -18,7 +54,7 @@ fn main() {
     for line in file.into_inner() {
         match line.as_rule() {
             parser::Rule::mov => {
-                println!("mov:{:?}", line);
+                //println!("mov:{:?}", line);
                 let mut inner_rule = line.into_inner();
                 handle_mov(&mut inner_rule);
             }
