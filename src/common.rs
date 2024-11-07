859d54858d28c97299edb744b3166a2fb5d331ca
Gioh Kim <gurugio@linecorp.com>
2024-11-07 10:01:03 +0900

    split common library file with macro
    make a separate file for common library

---
+++ src/common.rs
@@ -0,0 +1,29 @@
+#[macro_export]
+macro_rules! handle_instruction_one {
+    ($mod:ident, $func:ident) => {
+        fn $func(operands: &mut Pairs<Rule>) {
+            let first_operand = operands.next().unwrap();
+            $mod::$func(first_operand);
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! call_handler_two {
+    ($mod:ident, $func:ident) => {
+        fn $func(operands: &mut Pairs<Rule>) {
+            let first_operand = operands.next().unwrap();
+            let second_operand = operands.next().unwrap();
+            $mod::$func(first_operand, second_operand);
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! define_handler_two {
+    ($func:ident, $first:ident, $second:ident, $body:block) => {
+        pub fn $func($first: Pair<Rule>, $second: Pair<Rule>) {
+            $body
+        }
+    };
+}
