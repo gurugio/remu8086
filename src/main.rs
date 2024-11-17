fe568be0fea492fd53d0537bf352258bab2e9a59
Gioh Kim <gurugio@linecorp.com>
2024-11-17 16:51:17 +0900

    cpu registers

--- src/main.rs
+++ src/main.rs
@@ -8,12 +8,11 @@ mod parser;
 
 use paste::paste;
 use pest::Parser;
-use std::fs;
 use std::sync::Mutex;
 
 use actix_cors::Cors;
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
-use serde_json::json;
+use serde_json::{json, Value};
 
 /*async fn handle_step(req_body: String) -> impl Responder {
     let registers = json!({
@@ -42,14 +41,39 @@ impl AssemblyServer {
     fn handle_instruction(&mut self, line: &str) -> Result<(), String> {
         // TODO: parse one instruction and change status of CPU and memory
         println!("Handle instruction:{}", line);
+        let v: Value = serde_json::from_str(line).unwrap();
+        let v = v["code"].as_str().unwrap();
+
+        let instruction = parser::AssemblyParser::parse(parser::Rule::instruction, v)
+            .unwrap()
+            .next()
+            .unwrap();
+
+        match instruction.as_rule() {
+            parser::Rule::mov => {
+                caller_two!(mov, self.cpu, self.memory, instruction);
+            }
+            parser::Rule::org => {
+                caller_one!(org, self.cpu, self.memory, instruction);
+            }
+            parser::Rule::add => {
+                caller_two!(add, self.cpu, self.memory, instruction);
+            }
+            _ => println!("NOT implemented yet:{}", line),
+        }
+        println!("After instruction: {:?}", self.cpu);
+
         Ok(())
     }
 
     fn get_cpu(&self) -> serde_json::Value {
         // TODO: change CPU status into
+        println!("return: {:?}", self.cpu);
         serde_json::json!({
-            "AX": "0000",
-            "BX": "0000"
+            "AX": self.cpu.get_register("ax").unwrap().to_string(),
+            "BX": self.cpu.get_register("bx").unwrap().to_string(),
+            "CX": self.cpu.get_register("cx").unwrap().to_string(),
+            "DX": self.cpu.get_register("dx").unwrap().to_string(),
         })
     }
 
@@ -66,13 +90,14 @@ async fn handle_step(req_body: String, data: web::Data<AssemblyServerLock>) -> i
     let mut server = data.server.lock().unwrap();
     server.handle_instruction(&req_body).unwrap();
 
-    HttpResponse::Ok().json({
+    /*HttpResponse::Ok().json({
         // 예시로 간단한 JSON 응답을 반환합니다.
         serde_json::json!({
             "AX": "0000",
             "BX": "0000"
         })
-    })
+    })*/
+    HttpResponse::Ok().json(server.get_cpu())
 }
 
 async fn handle_reload(req_body: String, _data: web::Data<AssemblyServerLock>) -> impl Responder {
@@ -93,7 +118,7 @@ async fn handle_reload(req_body: String, _data: web::Data<AssemblyServerLock>) -
 async fn main() -> std::io::Result<()> {
     println!("Rust web-server started at 127.0.0.1:8080");
 
-    let myserver = web::Data::new(AssemblyServerLock {
+    let myserverdata = web::Data::new(AssemblyServerLock {
         server: Mutex::new(AssemblyServer::new()),
     });
 
@@ -107,7 +132,7 @@ async fn main() -> std::io::Result<()> {
                     .allowed_headers(vec![actix_web::http::header::CONTENT_TYPE])
                     .max_age(3600),
             )
-            .app_data(myserver.clone())
+            .app_data(myserverdata.clone())
             .route("/step", web::post().to(handle_step))
             .route("/reload", web::post().to(handle_reload))
     })
@@ -119,6 +144,7 @@ async fn main() -> std::io::Result<()> {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::fs;
 
     #[test]
     fn test_main_rune_example_file() {
