2a2163bf65c3cfc924c1b492e0baa16f51915eea
Gioh Kim <gurugio@linecorp.com>
2024-12-03 23:44:02 +0900

    add: implement 16-bit operations

--- src/add.rs
+++ src/add.rs
@@ -40,11 +40,11 @@ e.g. add ax, [000c] 03 06 0c 00
 /// ADD r/m16, imm8 $83 xx000xxx (ModR/M byte)
 */
 
-fn do_add(cpu: &mut CpuContext, _memory: &mut Memory, reg: &str, l: u16, r: u16) {
+fn do_add16(cpu: &mut CpuContext, l: u16, r: u16) -> u16 {
     // work-around the overflow checker of Rust
     let l32: u32 = l as u32;
     let r32: u32 = r as u32 + l32;
-    let r_added = r32 as u16;
+    let r_added: u16 = (r32 & 0xffff) as u16;
 
     // Setting OF cases
     // 1. the sum of two numbers with the sign bit off yields a result number with the sign bit on.
@@ -67,58 +67,105 @@ fn do_add(cpu: &mut CpuContext, _memory: &mut Memory, reg: &str, l: u16, r: u16)
         cpu.reset_CF();
     }
 
-    cpu.set_register(reg, r_added).unwrap();
+    r_added
+}
+
+/// Maybe do_add16 and do_add8 could be unified as do_add(cpu, num_bit, l, r)
+fn do_add8(cpu: &mut CpuContext, l: u8, r: u8) -> u8 {
+    // work-around the overflow checker of Rust
+    let l32: u32 = l as u32;
+    let r32: u32 = r as u32 + l32;
+    let r_added: u8 = (r32 & 0xff) as u8;
+
+    // Setting OF cases
+    // 1. the sum of two numbers with the sign bit off yields a result number with the sign bit on.
+    // 2. the sum of two numbers with the sign bit on yields a result number with the sign bit off.
+    if (l & 0x80) == 0 && (r & 0x80) == 0 && (r_added & 0x80) == 0x80 {
+        cpu.set_OF();
+    } else if (l & 0x80) == 0x80 && (r & 0x80) == 0x80 && (r_added & 0x80) == 0 {
+        cpu.set_OF();
+    } else {
+        cpu.reset_OF();
+    }
+
+    // Setting CF cases
+    // 1. the addition of two numbers causes a carry out of the most significant (leftmost) bits added.
+    // 2. the subtraction of two numbers requires a borrow into the most significant (leftmost) bits subtracted.
+    // 2nd case will be implemented in sub instruction handler.
+    if (r32 & 0x80) == 0 && (r32 & 0x100) == 1 {
+        cpu.set_CF();
+    } else {
+        cpu.reset_CF();
+    }
+
+    r_added
+}
+
+fn assemble_add(first: &Pair<Rule>, second: &Pair<Rule>) -> Vec<u8> {
+    let mut v: Vec<u8> = Vec::new();
+    // TODO
+    v
 }
 
 define_handler_two!(add, first, second, cpu, memory, {
     if first.as_str() == "ax" && second.as_rule() == Rule::imm {
-        // Todo
+        let l: u16 = cpu.get_register(first.as_str()).unwrap();
+        let r = imm_to_num(&second).unwrap();
+        let v = do_add16(cpu, l, r);
+        let _ = cpu.set_register(first.as_str(), v);
     } else if first.as_str() == "al" && second.as_rule() == Rule::imm {
-        // Todo
+        // TODO
     } else {
         match (first.as_rule(), second.as_rule()) {
+            // There is no mem-mem operation for ALL instruction.
             // 8/16-bit each has 5 operations. Total 10 operations.
             // reg-reg
             // reg-mem
             // reg-imm
             // mem-reg
             // mem-imm
-            // There is no mem-mem operation for ALL instruction.
             (Rule::reg16, Rule::reg16) => {
-                cpu.set_register(first.as_str(), cpu.get_register(second.as_str()).unwrap())
-                    .unwrap();
                 let l: u16 = cpu.get_register(first.as_str()).unwrap();
                 let r: u16 = cpu.get_register(second.as_str()).unwrap();
-                do_add(cpu, memory, first.as_str(), l, r);
+                let v = do_add16(cpu, l, r);
+                let _ = cpu.set_register(first.as_str(), v).unwrap();
             }
             (Rule::reg16, Rule::imm) => {
-                let r = imm_to_num(&second).unwrap();
                 let l = cpu.get_register(first.as_str()).unwrap();
-                do_add(cpu, memory, first.as_str(), l, r);
+                let r = imm_to_num(&second).unwrap();
+                let v = do_add16(cpu, l, r);
+                let _ = cpu.set_register(first.as_str(), v).unwrap();
             }
             (Rule::reg16, Rule::mem16) => {
                 let address = mem_to_num(&second).unwrap();
-                let v1 = memory.read16(address);
-                let v2 = cpu.get_register(first.as_str()).unwrap();
-                cpu.set_register(first.as_str(), v1 + v2).unwrap();
+                let l = memory.read16(address);
+                let r = cpu.get_register(first.as_str()).unwrap();
+                let v = do_add16(cpu, l, r);
+                let _ = cpu.set_register(first.as_str(), v).unwrap();
             }
             (Rule::reg8, Rule::reg8) => {
-                // Todo
+                // TODO
             }
             (Rule::reg8, Rule::imm) => {
-                // Todo
+                // TODO
             }
             (Rule::reg8, Rule::mem8) => {
                 // Todo
             }
             (Rule::mem16, Rule::reg16) => {
                 let address = mem_to_num(&first).unwrap();
-                let v1 = cpu.get_register(second.as_str()).unwrap();
-                let v2 = memory.read16(address);
-                memory.write16(address, v1 + v2);
+                let l = memory.read16(address);
+                let r = cpu.get_register(first.as_str()).unwrap();
+                let v = do_add16(cpu, l, r);
+                memory.write16(address, v);
             }
             (Rule::mem16, Rule::imm) => {
                 // Todo
+                let address = mem_to_num(&first).unwrap();
+                let l = memory.read16(address);
+                let r = imm_to_num(&second).unwrap();
+                let v = do_add16(cpu, l, r);
+                memory.write16(address, v);
             }
             (Rule::mem8, Rule::reg8) => {
                 // Todo
@@ -138,20 +185,19 @@ mod tests {
     #[test]
     fn test_add_overflow() {
         let mut cpu = CpuContext::boot();
-        let mut memory = Memory::boot();
 
         // Plus + Plus = Minus => Overflow error!
-        do_add(&mut cpu, &mut memory, "ax", u16::MAX / 2, u16::MAX / 2);
+        do_add16(&mut cpu, u16::MAX / 2, u16::MAX / 2);
         assert_ne!(0, cpu.get_OF());
 
         // 0xffff + 1 = 0x10000 => 0x0 as u16.
         // There is no overflow because -1 + 1 = 0.
         // But there is a carry.
-        do_add(&mut cpu, &mut memory, "ax", u16::MAX, 1);
+        do_add16(&mut cpu, u16::MAX, 1);
         assert_eq!(0, cpu.get_OF());
 
         // Plus + Plus = Minus => Overflow error!
-        do_add(&mut cpu, &mut memory, "ax", 0x7fff, 1);
+        do_add16(&mut cpu, 0x7fff, 1);
         assert_ne!(0, cpu.get_OF());
     }
 
